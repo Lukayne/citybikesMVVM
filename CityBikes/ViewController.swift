6893682b36597f6365cbce791a6c1e42c509ba94
Richard Smith <richarddevsmith@gmail.com>
2017-12-15 14:23:08 +0100

    Initial Commit

---
+++ CityBikes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityBikes
+//
+//  Created by Richard Smith on 2017-12-15.
+//  Copyright © 2017 Richard Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
